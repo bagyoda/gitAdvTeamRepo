5335a1f411f422b5d939ca856e1d1c61fce455f7
bberrci <b.berrci@gmail.com>
2019-09-16 16:23:01 +0200

    d4

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
 public class Main {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
+HEAD
+HEAD
   public void count(){
     System.out.println("31" +
             "");
-=======
 
   public void greet(String hello) {
   System.out.println("Greetings");
->>>>>>> c3815969321df5ecf50ef65e61b6e3b2c1df3617
+  c3815969321df5ecf50ef65e61b6e3b2c1df3617
   }
->>>>>>> 50f0d74adee3a74863c468e3df0fb2a1d6e6d73e
+ 50f0d74adee3a74863c468e3df0fb2a1d6e6d73e
 }
